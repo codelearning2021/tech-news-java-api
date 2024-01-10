ecac66c7b01438624b2d1917550a690f48f9aa33
Nate <nathan.evan.johnson@gmail.com>
2024-01-10 06:03:49 -0800

    trying again

---
+++ technews/tech-news-java-api/src/main/java/VariablesPractice.java
@@ -0,0 +1,12 @@
+public class VariablesPractice {
+    public static void main(String[] args) {
+            int a = 8;
+            double b = 33.2;
+            double sum = a + b;
+
+            String c = "Panda";
+            String concat = a + c;
+                    System.out.println(sum);
+                    System.out.println(concat);
+    }
+}
